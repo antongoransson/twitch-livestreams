3ac97f79e0f850fd59f2d8b1c00a02902dcfeed4
Anton Göransson <anton.goransson@telavox.com>
2019-10-16 12:45:57 +0200

    Refactor style to css file and refactor JS file

--- popup/fetchData.js
+++ popup/fetchData.js
@@ -9,22 +9,20 @@ async function doGetRequest(url) {
 
 async function getUserId() {
   const url = "https://api.twitch.tv/helix/users?login=";
-  const storage = await browser.storage.local.get();
-  const response = await doGetRequest(url + storage.twitchUsername);
-  if (response.data && response.data.length > 0) {
-    return response.data[0].id;
+  const { twitchUsername } = await browser.storage.local.get();
+  if (twitchUsername === undefined) {
+    return null;
+  }
+  const userInfo = await doGetRequest(url + twitchUsername);
+  if (userInfo.data && userInfo.data.length > 0) {
+    return userInfo.data[0].id;
   }
   return null;
 }
 
-function getFollowingStreamsURLString(data) {
-  return (
-    "https://api.twitch.tv/helix/streams?" +
-    data
-      .map(stream => `&user_id=${stream.to_id}`)
-      .join("")
-      .substring(1)
-  );
+function getFollowedStreamsUrl(data) {
+  const streamIds = getUrlParametersAsString(data, "user_id", "to_id");
+  return "https://api.twitch.tv/helix/streams?" + streamIds;
 }
 
 function groupLiveStreamsById(liveStreams) {
@@ -47,65 +45,79 @@ function createStreamLink(stream) {
   return link;
 }
 
-async function getGameNames(liveStreams) {
-  const repsonse = await doGetRequest(
-    "https://api.twitch.tv/helix/games?" +
-      liveStreams
-        .map(stream => `&id=${stream.game_id}`)
-        .join("")
-        .substring(1)
+function createSpanGameTitle(gameName) {
+  const textSpan = document.createElement("span");
+  textSpan.setAttribute("class", "game-title");
+  textSpan.appendChild(document.createTextNode(gameName));
+  return textSpan;
+}
+
+function showErrorMessage() {
+  document.getElementById("error").setAttribute("class", "");
+  const errorText = document.createTextNode(
+    "Make sure you have put the correct username in the extension settings"
   );
-  return repsonse.data.reduce((acc, curr) => {
+  document.getElementById("error").appendChild(errorText);
+}
+
+function getUrlParametersAsString(liveStreams, attrName, keyName) {
+  return liveStreams
+    .map((stream, i) => `${i > 0 ? "&" : ""}${attrName}=${stream[keyName]}`)
+    .join("");
+}
+
+async function getGameNames(liveStreams) {
+  const gameIds = getUrlParametersAsString(liveStreams, "id", "game_id");
+  const gamesUrl = "https://api.twitch.tv/helix/games?" + gameIds;
+  const gamesInfo = await doGetRequest(gamesUrl);
+  return gamesInfo.data.reduce((acc, curr) => {
     acc[curr.id] = curr.name;
     return acc;
   }, {});
 }
 
+async function getLiveStreams(fromId) {
+  let allLiveStreams = [];
+  let pagination = "";
+  let followedStreams;
+  do {
+    const followedStreamsUrl = `https://api.twitch.tv/helix/users/follows?from_id=${fromId}&first=100${
+      pagination ? "&after=" + pagination.cursor : ""
+    }`;
+    followedStreams = await doGetRequest(followedStreamsUrl);
+    pagination = followedStreams.pagination;
+    const liveStreamsUrl = getFollowedStreamsUrl(followedStreams.data);
+    const liveStreams = await doGetRequest(liveStreamsUrl);
+    allLiveStreams = [...allLiveStreams, ...liveStreams.data];
+  } while (
+    followedStreams.total > 100 &&
+    followedStreams.data &&
+    followedStreams.data.length === 100
+  );
+
+  allLiveStreams.sort((a, b) => b.viewer_count - a.viewer_count);
+  return allLiveStreams;
+}
+
 async function getData() {
   const fromId = await getUserId();
   if (fromId === null) {
-    document.getElementById("error").setAttribute("class", "");
-    document
-      .getElementById("error")
-      .appendChild(
-        document.createTextNode(
-          "Make sure you have put the correct username in the extension settings"
-        )
-      );
+    showErrorMessage();
     return;
   }
   document.getElementById("error").setAttribute("class", "hidden");
-  let response = await doGetRequest(
-    `https://api.twitch.tv/helix/users/follows?from_id=${fromId}&first=100`
-  );
-  let { data, pagination } = response;
-  let requestString = getFollowingStreamsURLString(data);
 
-  let liveStreams = await doGetRequest(requestString);
-
-  if (response.total > 100) {
-    response = await doGetRequest(
-      "https://api.twitch.tv/helix/users/follows?from_id=31127228&first=100&after=" +
-        pagination.cursor
-    );
-    requestString = getFollowingStreamsURLString(response.data);
-    let liveStreams2 = await doGetRequest(requestString);
-    liveStreams = [...liveStreams.data, ...liveStreams2.data];
-  }
-  liveStreams.sort((a, b) => b.viewer_count - a.viewer_count);
-
+  const liveStreams = await getLiveStreams(fromId);
   const liveStreamsGroupedByGameId = groupLiveStreamsById(liveStreams);
   const gameNames = await getGameNames(liveStreams);
-  const element = document.getElementById("dropdown-content");
+  const streamList = document.getElementById("dropdown-content");
 
   let sortedGames = Object.keys(liveStreamsGroupedByGameId).sort((a, b) =>
     (gameNames[a] || "").localeCompare(gameNames[b] || "")
   );
 
   sortedGames.forEach(key => {
-    const textSpan = document.createElement("span");
-    textSpan.setAttribute("class", "game-title");
-    textSpan.appendChild(document.createTextNode(gameNames[key]));
+    const textSpan = createSpanGameTitle(gameNames[key]);
     const div = document.createElement("div");
     div.setAttribute("class", "game-category");
     div.appendChild(textSpan);
@@ -116,39 +128,8 @@ async function getData() {
       span.appendChild(document.createTextNode(stream.viewer_count));
       span.setAttribute("class", "stream-view-count");
       div.appendChild(span);
-      //div.append([link, span]);
     });
-    element.appendChild(div);
+    streamList.appendChild(div);
   });
 }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   browser.runtime
-//     .sendMessage({
-//       type: "getText"
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//       const { gameNames, sortedGames, liveStreamsGroupedByGameId } = data;
-//       const element = document.getElementById("dropdown-content");
-//       sortedGames.forEach(key => {
-//         const textSpan = document.createElement("span");
-//         textSpan.setAttribute("class", "game-title");
-//         textSpan.appendChild(document.createTextNode(gameNames[key]));
-//         const div = document.createElement("div");
-//         div.setAttribute("class", "game-category");
-//         div.appendChild(textSpan);
-//         liveStreamsGroupedByGameId[key].forEach(stream => {
-//           const link = createStreamLink(stream);
-//           div.appendChild(link);
-//           const span = document.createElement("span");
-//           span.appendChild(document.createTextNode(stream.viewer_count));
-//           span.setAttribute("class", "stream-view-count");
-//           div.appendChild(span);
-//           //div.append([link, span]);
-//         });
-//         element.appendChild(div);
-//       });
-//     });
-// });
 getData();
