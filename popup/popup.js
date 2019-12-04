7f1fe23044ad1e18e52d827545e391bb0c1f95c1
antongoransson <anton.goransson95@gmail.com>
2019-12-04 15:29:09 +0100

    Add refersh button to force refresh streams

--- popup/popup.js
+++ popup/popup.js
@@ -2,21 +2,25 @@ const DEFAULT_ERROR_MESSAGE =
   "Make sure you have put the correct username in the extension settings";
 const BOX_ART_WIDTH = 60;
 const BOX_ART_HEIGHT = 80;
+const STREAM_LIST = document.getElementById("dropdown-content");
+const ERROR_MESSAGE = document.getElementById("error");
+const LOADER = document.getElementById("spinning-loader");
 
 function createGameContainer(gameName, boxArtUrl) {
   const gameContainerTemplate = document.getElementsByTagName("template")[0];
   const gameContainerClone = gameContainerTemplate.content.cloneNode(true);
   const gameTitleLink = gameContainerClone.querySelector("#game-title");
   const gameBoxLogo = gameContainerClone.querySelector("#box-logo");
-
   const gameUrl = `https://www.twitch.tv/directory/game/${gameName}`;
 
   gameTitleLink.textContent = gameName;
+  gameTitleLink.setAttribute("title", gameName);
   gameTitleLink.setAttribute("href", gameUrl);
   boxArtUrl = boxArtUrl
     .replace("{width}", BOX_ART_WIDTH)
     .replace("{height}", BOX_ART_HEIGHT);
   gameBoxLogo.setAttribute("src", boxArtUrl);
+  gameBoxLogo.setAttribute("title", gameName);
   gameContainerClone
     .querySelector("#game-logo-link")
     .setAttribute("href", gameUrl);
@@ -33,18 +37,64 @@ function createStreamContainer(stream) {
   const trimmedUserName = stream.user_name.replace(/\s+/g, "");
   streamLink.childNodes[0].textContent = stream.user_name;
   streamLink.setAttribute("href", `https://www.twitch.tv/${trimmedUserName}`);
-
   streamContainer.querySelector("#tooltiptext").textContent = stream.title;
   viewCount.textContent = stream.viewer_count;
   return streamContainer;
 }
 
 function showErrorMessage(message) {
-  document.getElementById("error").setAttribute("class", "");
+  ERROR_MESSAGE.setAttribute("class", "");
   const errorText = message || DEFAULT_ERROR_MESSAGE;
   const errorTextNode = document.createTextNode(errorText);
-  document.getElementById("error").appendChild(errorTextNode);
-  document.getElementById("spinning-loader").setAttribute("class", "hidden");
+  ERROR_MESSAGE.appendChild(errorTextNode);
+  LOADER.setAttribute("class", "hidden");
+}
+
+function registerRefreshButton() {
+  const refreshIcon = document.querySelector("#refresh");
+  refreshIcon.setAttribute("class", "refresh-button");
+  refreshIcon.addEventListener("click", refreshStreams);
+}
+
+async function refreshStreams() {
+  const BACKGROUND_PAGE = await browser.runtime.getBackgroundPage();
+  const refreshButton = document.getElementById("refresh-button");
+
+  refreshButton.setAttribute("class", "hidden");
+  ERROR_MESSAGE.setAttribute("class", "hidden");
+  const toBeRemoved = [];
+  for (let i = 0; i < STREAM_LIST.children.length; i++) {
+    if (STREAM_LIST.children[i].className === "game-container") {
+      toBeRemoved.push(STREAM_LIST.children[i]);
+    }
+  }
+  LOADER.setAttribute("class", "lds-hourglass");
+  toBeRemoved.forEach(c => STREAM_LIST.removeChild(c));
+  const result = await BACKGROUND_PAGE.getAllData();
+  refreshButton.setAttribute("class", "");
+  createStreamList(result);
+}
+
+function createStreamList(result) {
+  const { liveFollowedStreams, gameNames } = result;
+  if (Object.keys(liveFollowedStreams).length === 0) {
+    showErrorMessage("No followed streams are live");
+    return;
+  }
+  const sortedGames = Object.keys(liveFollowedStreams).sort((a, b) =>
+    gameNames[a].name.localeCompare(gameNames[b].name)
+  );
+
+  sortedGames.forEach(key => {
+    const { name: gameName, boxArtUrl } = gameNames[key];
+    const gameContainerClone = createGameContainer(gameName, boxArtUrl);
+    const gameCategory = gameContainerClone.querySelector("#game-info");
+    liveFollowedStreams[key].forEach(stream => {
+      gameCategory.appendChild(createStreamContainer(stream));
+    });
+    STREAM_LIST.appendChild(gameContainerClone);
+  });
+  LOADER.setAttribute("class", "hidden");
 }
 
 async function getSession() {
@@ -62,33 +112,13 @@ async function getSession() {
 }
 
 async function main() {
-  document.getElementById("error").setAttribute("class", "hidden");
+  ERROR_MESSAGE.setAttribute("class", "hidden");
   const result = await getSession();
   if (!result) {
     return;
   }
-  const { liveFollowedStreams, gameNames } = result;
-  if (Object.keys(liveFollowedStreams).length === 0) {
-    showErrorMessage("No followed streams are live");
-    return;
-  }
-  const sortedGames = Object.keys(liveFollowedStreams).sort((a, b) =>
-    gameNames[a].name.localeCompare(gameNames[b].name)
-  );
-
-  const streamList = document.getElementById("dropdown-content");
-  sortedGames.forEach(key => {
-    const { name: gameName, boxArtUrl } = gameNames[key];
-    const gameContainerClone = createGameContainer(gameName, boxArtUrl);
-    const gameCategory = gameContainerClone.querySelector("#game-info");
-
-    liveFollowedStreams[key].forEach(stream => {
-      gameCategory.appendChild(createStreamContainer(stream));
-    });
-
-    streamList.appendChild(gameContainerClone);
-  });
-  document.getElementById("spinning-loader").setAttribute("class", "hidden");
+  createStreamList(result);
+  registerRefreshButton();
 }
 
 main();
